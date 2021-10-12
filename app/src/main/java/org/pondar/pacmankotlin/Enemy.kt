f07737d669ab67d709167144f028e7fa6a8dcce5
KN <nicholas.kevin96@gmail.com>
2021-10-12 20:30:56 +0200

    somechange

--- app/src/main/java/org/pondar/pacmankotlin/Enemy.kt
+++ app/src/main/java/org/pondar/pacmankotlin/Enemy.kt
@@ -11,25 +11,20 @@ class Enemy ( h: Int, w: Int, bitmap: Bitmap, pacY:Int, pacX:Int) {
     var x: Int
     var xDir:Int
     var yDir:Int
-
-
     var EnemyBitmap: Bitmap = bitmap
 
-
-
     init {
-        xDir = Random.nextInt(0,1)
-        yDir = Random.nextInt(0,1)
-        Log.d("hej",h.toString())
-        y = Random.nextInt(0, h-200)
-        x = Random.nextInt(600, w-200)
-      y = pacY+100
-        x = pacX+100
+        xDir = 1
+        yDir = 0
+        y = 0
+        x = 1 //int er ikke nullable
+
     }
 
     init {
         var yTry = Random.nextInt(0, h - 200)
         var xTry = Random.nextInt(0, w - 200)
+
         if (yTry != pacY && xTry != pacX)
             y = yTry+80
             x = xTry+80
